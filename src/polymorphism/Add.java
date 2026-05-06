6bddc55037b571f96c685cf8944c5490d643258a
Kirti Singh <kirtisingh1497@gmail.com>
2026-05-06 20:55:11 +0530

    Changes saved

--- src/polymorphism/Add.java
+++ src/polymorphism/Add.java
@@ -1,5 +1,11 @@
 package polymorphism;
 
+//polumorphism is the ability of an object to take on many forms. It is one of the core concepts of object-oriented programming.
+// There are two types of polymorphism in java: compile time polymorphism and runtime polymorphism
+// Compile time polymorphism is achieved through method overloading and operator overloading.
+// in testing, we can use polymorphism to create test cases that can be reused for different types of objects. 
+// This can help to reduce the amount of code that we need to write and maintain.
+
 // Method Overloading Example
 public class Add {
     int a=10;
@@ -22,4 +28,9 @@ public class Add {
 //java checker will check the method signature to determine which method to call. 
 // If there are multiple methods with the same name and the same number of parameters, then the compiler will throw an error.
 //error: reference to sum is ambiguous
+
+
+//comstructor overloading is a feature in java where we can have multiple constructors with the same name but different parameters.
+// It is also known as compile time polymorphism. The constructor to be called is determined at compile time based on the number and type of arguments passed to the constructor.
+
 
