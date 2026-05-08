06de468b921d1fa55f8fd9597ecb1956da2e62d3
Kirti Singh <kirtisingh1497@gmail.com>
2026-05-08 13:38:07 +0530

    class method added

--- src/abstractaction/InterfaceDemo.java
+++ src/abstractaction/InterfaceDemo.java
@@ -18,12 +18,17 @@ public class  InterfaceDemo implements Shape {
     public void circle() {
         System.out.println("Circle: implemented method");
     } 
+
+    void triangle() {
+        System.out.println("Triangle: class method");
+    }
      public static void main(String[] args) {
         //scenario 1: creating an instance of the interface and calling the methods
       InterfaceDemo demo = new InterfaceDemo();
       demo.square();
       Shape.rectangle();
       demo.circle();
+      demo.triangle();
     //   demo.rectangle(); // This will cause a compile-time error because rectangle() is a static method and cannot be called on an instance.
 
         //scenario 2: creating a reference of the interface type and calling the methods
@@ -31,6 +36,7 @@ public class  InterfaceDemo implements Shape {
         shape.square();
         Shape.rectangle();
         shape.circle();
+        // shape.triangle(); // This will cause a compile-time error because triangle() is not defined in the Shape interface.
 
 }
 }
